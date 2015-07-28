ce7d26d2137a00d12fe1bc9d9870db2eb5619d24
abdulkadir <abdulkadir.karabas@cmak.com>
2015-07-28 13:27:22 +0300

    Regulation code

--- app/src/main/java/com/wan/yalandan/app/DictionaryReader.java
+++ app/src/main/java/com/wan/yalandan/app/DictionaryReader.java
@@ -1,68 +1,58 @@
 package com.wan.yalandan.app;
 
 import android.content.Context;
-import android.net.Uri;
 import android.util.Log;
 
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
-import java.util.Scanner;
 
 /**
  * Created by abdulkadir.karabas on 27.07.2015.
  */
 public class DictionaryReader {
-    public int resourcesid;
-    public Context ctx;
-    public List<String> wordlist;
+    private final static String LOG_TAG = DictionaryReader.class.getCanonicalName();
+    public List<String> wordList;
 
-    DictionaryReader(int resourcesid,Context ctx) {
-        this.resourcesid = resourcesid;
-        this.ctx = ctx;
+    DictionaryReader(int resourcesId, Context ctx) {
         try {
-            getWords(ctx);
+            getWords(ctx, resourcesId);
         } catch (FileNotFoundException e) {
-            e.printStackTrace();
+            Log.e(LOG_TAG, "Given resource id is invalid,skipping");
         }
     }
 
-    public void getWords(Context ctx) throws FileNotFoundException {
-
-        InputStream inputStream = ctx.getResources().openRawResource(resourcesid);
-        InputStreamReader inputreader = new InputStreamReader(inputStream);
-        BufferedReader buffreader = new BufferedReader(inputreader);
+    private void getWords(Context ctx, int resourcesId) throws FileNotFoundException {
+        Log.i(LOG_TAG, "File read process beginning");
+        InputStream inputStream = ctx.getResources().openRawResource(resourcesId);
+        InputStreamReader inputReader = new InputStreamReader(inputStream);
+        BufferedReader buffer = new BufferedReader(inputReader);
         String line;
 
-        wordlist = new ArrayList<>();
+        wordList = new ArrayList<>();
         try {
-            while ((line = buffreader.readLine()) != null) {
-                if(line.contains("'") || line.length()<2)continue;
-                wordlist.add(line);
+            while ((line = buffer.readLine()) != null) {
+                if (line.contains("'") || line.length() < 2) continue;
+                wordList.add(line);
             }
         } catch (IOException e) {
-            Log.e("FILE","File reader process is fail");
-
-        }
-        finally {
-            Log.i("FILE","File read process is done");
+            Log.e(LOG_TAG, "File reader process has failed");
+        } finally {
+            Log.i(LOG_TAG, "File read process is done");
         }
     }
 
-    public List<String> get4RandomWord() {
-
-        List<String> randwords = new ArrayList<>(4);
+    public List<String> get4RandomWords() {
+        List<String> randomWords = new ArrayList<>(4);
         Random rnd = new Random();
-        int size = wordlist.size();
-        while (randwords.size() < 4) {
-
-            int a = Math.abs(rnd.nextInt()%size);
-            if (!randwords.contains(wordlist.get(a)))
-                randwords.add(wordlist.get(a));
-
+        int size = wordList.size();
+        while (randomWords.size() < 4) {
+            int a = Math.abs(rnd.nextInt() % size);
+            if (!randomWords.contains(wordList.get(a)))
+                randomWords.add(wordList.get(a));
         }
 
-        return randwords;
+        return randomWords;
     }
 }
--- app/src/main/java/com/wan/yalandan/app/MainActivity.java
+++ app/src/main/java/com/wan/yalandan/app/MainActivity.java
@@ -1,21 +1,9 @@
 package com.wan.yalandan.app;
 
 import android.app.Activity;
-import android.content.Context;
-import android.database.Cursor;
-import android.net.Uri;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
-
-import java.io.*;
-import java.nio.channels.Channels;
-import java.util.List;
-import java.util.Scanner;
 
 
 public class MainActivity extends Activity {
@@ -34,9 +22,9 @@ public class MainActivity extends Activity {
         final DictionaryReader dr = new DictionaryReader(R.raw.american_english,getBaseContext());
         final TextView tv = (TextView) findViewById(R.id.textView);
 
-        tv.setText(dr.get4RandomWord().toString());
+        tv.setText(dr.get4RandomWords().toString());
         tv.setText(getBaseContext().getResources().openRawResource(R.raw.american_english).toString());
         Button btn = (Button) findViewById(R.id.btnAnswer);
-        btn.setOnClickListener(v -> tv.setText(dr.get4RandomWord().toString()));
+        btn.setOnClickListener(v -> tv.setText(dr.get4RandomWords().toString()));
     }
 }
