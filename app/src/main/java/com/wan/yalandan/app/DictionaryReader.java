ef5dfdd600bc5db5a27bcda7c75ef0d20f895fee
abdulkadir <abdulkadir.karabas@cmak.com>
2015-07-28 10:49:18 +0300

    Regulation code

--- app/src/main/java/com/wan/yalandan/app/DictionaryReader.java
+++ app/src/main/java/com/wan/yalandan/app/DictionaryReader.java
@@ -15,34 +15,44 @@ import java.util.Scanner;
  */
 public class DictionaryReader {
     public int resourcesid;
+    public Context ctx;
     public List<String> wordlist;
 
-    DictionaryReader(int resourcesid) {
+    DictionaryReader(int resourcesid,Context ctx) {
         this.resourcesid = resourcesid;
+        this.ctx = ctx;
+        try {
+            getWords(ctx);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
-    public void getWords(InputStream _inputStream) throws FileNotFoundException {
-        InputStream inputStream = _inputStream;
+    public void getWords(Context ctx) throws FileNotFoundException {
+
+        InputStream inputStream = ctx.getResources().openRawResource(resourcesid);
         InputStreamReader inputreader = new InputStreamReader(inputStream);
         BufferedReader buffreader = new BufferedReader(inputreader);
         String line;
 
-        wordlist = new ArrayList<String>();
+        wordlist = new ArrayList<>();
         try {
             while ((line = buffreader.readLine()) != null) {
-                if(line.length()<2)continue;
-                if(line.contains("'"))continue;
+                if(line.contains("'") || line.length()<2)continue;
                 wordlist.add(line);
             }
         } catch (IOException e) {
-            Log.e("Dosya okuma hatas?","DictionaryReader");
-            return;
+            Log.e("FILE","File reader process is fail");
+
+        }
+        finally {
+            Log.i("FILE","File read process is done");
         }
     }
 
     public List<String> get4RandomWord() {
-        List<String> randwords = new ArrayList<String>();
 
+        List<String> randwords = new ArrayList<>(4);
         Random rnd = new Random();
         int size = wordlist.size();
         while (randwords.size() < 4) {
