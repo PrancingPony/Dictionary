10b0c21c39d7e7e9b4594cef5eb1c03b3a5eadb3
emrec <emre.can@cmak.com>
2015-07-31 18:26:38 +0300

    improve logic and clean up

--- app/src/main/java/com/wan/yalandan/app/util/XmlParser.java
+++ app/src/main/java/com/wan/yalandan/app/util/XmlParser.java
@@ -28,10 +28,10 @@ public class XmlParser {
     private final String TAG_ANTONYM = "ant";
     private final String TAG_SENSE = "sens";
     private final String TAG_IT = "it";
-    private Context context=null;
+    private Context context = null;
 
     public XmlParser(Context context) {
-        this.context=context;
+        this.context = context;
     }
 
     public Word getWordData(String uri) {
@@ -56,36 +56,34 @@ public class XmlParser {
 
     private Word parseXML(XmlPullParser parser) throws XmlPullParserException, IOException {
         int eventType = parser.getEventType();
-        HashMap<String, String> hmWordFeature = new HashMap<>();
-        List<String> tags = new ArrayList<>(2);
+        HashMap<String, String> tagTextMap = new HashMap<>();
         String currentTag = null;
-
         while (eventType != XmlPullParser.END_DOCUMENT) {
             switch (eventType) {
-                case XmlPullParser.START_DOCUMENT:
-                    break;
-
                 case XmlPullParser.START_TAG:
                     if (TAG_IT.equals(parser.getName())) {
                         parser.next();
                         StringBuilder appendedText = new StringBuilder(parser.getText());
                         parser.next();
                         parser.next();
+                        appendedText.append(tagTextMap.get(currentTag));
                         appendedText.append(parser.getText());
-                        hmWordFeature.put(currentTag, hmWordFeature.get(currentTag) + appendedText);
+                        tagTextMap.put(currentTag, appendedText.toString());
                     }
                     currentTag = parser.getName();
-                    tags.add(parser.getName());
-                    eventType = parser.next();
-                    continue;
-
-                case XmlPullParser.TEXT:
-                    DetermineTagsGetContents(parser, currentTag, hmWordFeature, tags);
                     break;
-
+                case XmlPullParser.TEXT:
+                    determineTagsAndGetContents(parser, currentTag, tagTextMap);
+                    break;
                 case XmlPullParser.END_TAG:
                     if (TAG_SENSE.equals(parser.getName())) {
-                        return new Word(hmWordFeature.get(TAG_HEADWORD), hmWordFeature.get(TAG_FUNCTIONAL_LABEL), hmWordFeature.get(TAG_MEANING_CORE), hmWordFeature.get(TAG_ILLUSTRATIVE_SENTENCE), SplitTrimString(hmWordFeature.get(TAG_SYNONYM)), SplitTrimString(hmWordFeature.get(TAG_RELATED_WORDS)), SplitTrimString(hmWordFeature.get(TAG_ANTONYM)));
+                        return new Word(tagTextMap.get(TAG_HEADWORD),
+                                tagTextMap.get(TAG_FUNCTIONAL_LABEL),
+                                tagTextMap.get(TAG_MEANING_CORE),
+                                tagTextMap.get(TAG_ILLUSTRATIVE_SENTENCE),
+                                SplitTrimString(tagTextMap.get(TAG_SYNONYM)),
+                                SplitTrimString(tagTextMap.get(TAG_RELATED_WORDS)),
+                                SplitTrimString(tagTextMap.get(TAG_ANTONYM)));
                     }
                     break;
             }
@@ -94,39 +92,41 @@ public class XmlParser {
         return null;
     }
 
-    public void DetermineTagsGetContents(XmlPullParser parser, String currentTag, HashMap<String, String> hmWordFeature, List<String> tags) throws IOException, XmlPullParserException {
-
+    public void determineTagsAndGetContents(XmlPullParser parser, String currentTag, HashMap<String, String> tagTextMap) throws IOException, XmlPullParserException {
         switch (currentTag) {
             case TAG_HEADWORD:
-                hmWordFeature.put(TAG_HEADWORD, parser.getText());
+                tagTextMap.put(TAG_HEADWORD, parser.getText());
                 break;
             case TAG_FUNCTIONAL_LABEL:
-                hmWordFeature.put(TAG_FUNCTIONAL_LABEL, parser.getText());
+                tagTextMap.put(TAG_FUNCTIONAL_LABEL, parser.getText());
                 break;
             case TAG_MEANING_CORE:
-                hmWordFeature.put(TAG_MEANING_CORE, parser.getText());
+                tagTextMap.put(TAG_MEANING_CORE, parser.getText());
                 break;
             case TAG_ILLUSTRATIVE_SENTENCE:
-                hmWordFeature.put(TAG_ILLUSTRATIVE_SENTENCE, parser.getText());
+                tagTextMap.put(TAG_ILLUSTRATIVE_SENTENCE, parser.getText());
                 break;
             case TAG_SYNONYM:
-                hmWordFeature.put(TAG_SYNONYM, parser.getText());
+                tagTextMap.put(TAG_SYNONYM, parser.getText());
                 break;
             case TAG_RELATED_WORDS:
-                hmWordFeature.put(TAG_RELATED_WORDS, parser.getText());
+                tagTextMap.put(TAG_RELATED_WORDS, parser.getText());
                 break;
             case TAG_ANTONYM:
-                hmWordFeature.put(TAG_ANTONYM, parser.getText());
+                tagTextMap.put(TAG_ANTONYM, parser.getText());
                 break;
         }
     }
 
     public List<String> SplitTrimString(String listString) {
-        List<String> words = Arrays.asList(listString.split(","));
-        List<String> trimmedWords = new ArrayList<>();
-        for (String currentWord : words) {
-            trimmedWords.add(currentWord.trim());
+        if (listString != null) {
+            List<String> words = Arrays.asList(listString.split(","));
+            List<String> trimmedWords = new ArrayList<>();
+            for (String currentWord : words) {
+                trimmedWords.add(currentWord.trim());
+            }
+            return trimmedWords;
         }
-        return trimmedWords;
+        return new ArrayList<>();
     }
 }
