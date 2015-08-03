522b254c79123ca7af0b6a29600c9896e1b754d5
Oguzhang <oguzhan.gedik@cmak.com>
2015-08-03 18:18:39 +0300

    Merging process was completed by Oguzhan

--- app/src/main/java/com/wan/yalandan/app/MainActivity.java
+++ app/src/main/java/com/wan/yalandan/app/MainActivity.java
@@ -1,7 +1,6 @@
 package com.wan.yalandan.app;
 
 import android.app.Activity;
-import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
@@ -14,36 +13,22 @@ import java.util.Random;
 
 public class MainActivity extends Activity {
 
+    Button btnAnswer;
+    TextView tv;
+    DownloadFileProcess dfp;
+
     static List<RadioButton> radioButtons = new ArrayList<>(4);
     static ArrayList<Object> uriList = new ArrayList<>(4);
-    DownloadFileProcess dfp;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //----TEST CODE------------
+        init();
+
         DictionaryReader dr = new DictionaryReader(R.raw.american_english, getBaseContext());
         DownloadFileProcess.createFolder(getApplicationInfo().dataDir, "xmls");
-        final Button btnAnswer = (Button) findViewById(R.id.btnAnswer);
-        final TextView tv = (TextView) findViewById(R.id.textView);
-
-        dfp = new DownloadFileProcess(uri -> {
-            Log.d("CALLED URI", uri);
-        }, getBaseContext()
-        );
-
-        btnAnswer = (Button) findViewById(R.id.btnAnswer);
-
-        btnAnswer.setOnClickListener(v -> {
-
-            dfp.getWordUriFromApi("hi");
-        //-----------------------------
-        radioButtons.add((RadioButton) findViewById(R.id.radioButton));
-        radioButtons.add((RadioButton) findViewById(R.id.radioButton2));
-        radioButtons.add((RadioButton) findViewById(R.id.radioButton3));
-        radioButtons.add((RadioButton) findViewById(R.id.radioButton4));
-        //-----------------------------
 
         List<String> randomWords = dr.getRandomWords(4);
         Random random = new Random();
@@ -51,27 +36,23 @@ public class MainActivity extends Activity {
         randomWords = dr.getRandomWords(4);
         final List<String> finalRandomWords = randomWords;
 
+        dfp = new DownloadFileProcess(uri -> {
+            Log.d("CALLED URI", uri);
+            uriList.add(uri);
+            if (uriList.size() == 4) {
+                for (RadioButton radioButton : radioButtons) {
+                    radioButton.setText(uriList.get(radioButtons.indexOf(radioButton)).toString());
+                }
+            }
+        }, getBaseContext()
+        );
+
         btnAnswer.setOnClickListener(v -> {
             uriList.clear();
-            dfp = new DownloadFileProcess(
-                    uri -> {
-                        uriList.add(uri);
-                        if(uriList.size()==4) {
-                            for (RadioButton radioButton : radioButtons) {
-                                radioButton.setText(uriList.get(radioButtons.indexOf(radioButton)).toString());
-                            }
-                        }
-                        Log.d("URILIST", String.valueOf(uriList.size()));
-                    }
-                    , getBaseContext());
             for (String word : finalRandomWords) {
                 dfp.getWordUriFromApi(word);
             }
-
         });
-//
-
-        //----TEST END-------------
     }
 
     @Override
@@ -80,6 +61,14 @@ public class MainActivity extends Activity {
         dfp.unregisterReceiver();
     }
 
+    public void init() {
+        btnAnswer = (Button) findViewById(R.id.btnAnswer);
+        tv = (TextView) findViewById(R.id.textView);
 
+        radioButtons.add((RadioButton) findViewById(R.id.radioButton));
+        radioButtons.add((RadioButton) findViewById(R.id.radioButton2));
+        radioButtons.add((RadioButton) findViewById(R.id.radioButton3));
+        radioButtons.add((RadioButton) findViewById(R.id.radioButton4));
+    }
 }
 
