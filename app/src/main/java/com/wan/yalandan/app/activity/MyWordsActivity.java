69f4ab61ac79182fb0320bb30f1ebc24a6eb8f28
abdulkadir <abdulkadir.karabas@cmak.com>
2015-08-06 18:39:19 +0300

    Complete MyWordsActivity for random word select in user word list

--- app/src/main/java/com/wan/yalandan/app/activity/MyWordsActivity.java
+++ app/src/main/java/com/wan/yalandan/app/activity/MyWordsActivity.java
@@ -1,6 +1,76 @@
 package com.wan.yalandan.app.activity;
 
 import android.app.Activity;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import com.wan.yalandan.app.R;
+import com.wan.yalandan.app.data.DataStore;
+import com.wan.yalandan.app.model.Word;
+import com.wan.yalandan.app.util.WordUri;
+import com.wan.yalandan.app.util.XmlParser;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class MyWordsActivity extends Activity {
+    private XmlParser parser;
+    private Random random;
+    private TextView tvWord, tvMean;
+    private List<WordUri> userWordList;
+    private int indexOfCurrentWord;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mywords);
+        parser = new XmlParser();
+        random = new Random();
+        indexOfCurrentWord = 0;
+        tvWord = (TextView) findViewById(R.id.tvMyWordsWord);
+        tvMean = (TextView) findViewById(R.id.tvMyWordsMeaning);
+        Button btnNextWord = (Button) findViewById(R.id.btnMyWordsNextWord);
+        Button btnShowMean = (Button) findViewById(R.id.btnMyWordsShowMeaning);
+        btnNextWord.setOnClickListener(v -> nextWord());
+        btnShowMean.setOnClickListener(v -> showMeaning());
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        userWordList = new ArrayList<>();
+        DataStore dataStore = new DataStore(this);
+        Cursor cursor = dataStore.getWord(DataStore.ListName.USER_DEFINED);
+        while (cursor.moveToNext()) {
+            WordUri wordUri = new WordUri(cursor.getString(cursor.getColumnIndex(DataStore.TOKENWORDS_WORD)),
+                    cursor.getString(cursor.getColumnIndex(DataStore.TOKENWORDS_URI)),
+                    cursor.getLong(cursor.getColumnIndex(DataStore.TOKENWORDS_DATE)));
+            userWordList.add(wordUri);
+        }
+        cursor.close();
+        nextWord();
+    }
+
+    private int selectRandomWord() {
+        return Math.abs(random.nextInt()) % userWordList.size();
+    }
+
+    private void nextWord() {
+        indexOfCurrentWord = selectRandomWord();
+        tvWord.setText(userWordList.get(indexOfCurrentWord).word);
+        tvMean.setText("");
+    }
+
+    private void showMeaning() {
+        Word word = parser.getWordData(userWordList.get(indexOfCurrentWord).uri);
+        tvMean.setText(word.getMeaningCore());
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        userWordList.clear();
+    }
 }
---
+++ app/src/main/java/com/wan/yalandan/app/util/WordUri.java
@@ -0,0 +1,17 @@
+package com.wan.yalandan.app.util;
+
+/**
+ * Created by abdulkadir.karabas on 06.08.2015.
+ */
+public class WordUri {
+    public String word;
+    public String uri;
+    public long date;
+
+    public WordUri(String word, String uri, long date) {
+        this.word = word;
+        this.uri = uri;
+        this.date = date;
+
+    }
+}
